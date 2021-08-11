1303de71accb35a8114d761f481f18b86fb1c493
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-08-11 22:34:17 +0530

    Create File Handling.cpp

---
+++ C++ Introduction/File Handling.cpp
@@ -0,0 +1,10 @@
+Answer the following question:
+
+What is the default open mode if we use ifstream to open the file?
+  
+  
+
+Select your answer from the following options:
+ios::out
+ios::in    // -Correct
+ios::in | ios::out
