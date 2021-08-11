412a34c9247ee85e13a11932b8b6123fe750fef9
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-08-11 22:35:01 +0530

    Create Random Numbers.cpp

---
+++ C++ Introduction/Random Numbers.cpp
@@ -0,0 +1,16 @@
+Answer the question below:
+
+What will be the output of the following code?
+
+int main(){
+    cout<< rand()%1 <<endl;
+}
+
+
+Select your answer from the following options:
+1
+0        // - Correct
+Can't say
+  
+  
+  
