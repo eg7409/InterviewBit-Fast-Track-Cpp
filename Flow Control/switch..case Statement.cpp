0fa091df5bbdf31fb7ba19a1e551f9535cc27179
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-08-14 21:58:09 +0530

    Create switch..case Statement.cpp

---
+++ Flow Control/switch..case Statement.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    int w;
+    cin>>w;
+    // YOUR CODE GOES HERE
+
+    switch(w){
+        case 1:
+        cout<<"Monday";
+        break;
+
+        case 2:
+        cout<<"Tuesday";
+        break;
+
+        case 3:
+        cout<<"Wednesday";
+        break;
+
+        case 4:
+        cout<<"Thursday";
+        break;
+
+        case 5:
+        cout<<"Friday";
+        break;
+
+        case 6:
+        cout<<"Saturday";
+        break;
+
+        case 7:
+        cout<<"Sunday";
+        break;
+        
+    }
+    
+    return 0;
+}
