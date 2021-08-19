acd14ee65d9e2c6f1067873316b03e2cbe73f3b3
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-08-19 21:09:39 +0530

    Create Conditional or Ternary Expression.cpp

---
+++ Flow Control/Conditional or Ternary Expression.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()  {
+    int x, y;
+    cin>>x>>y;
+    // YOUR CODE GOES HERE
+
+    string a = (x<=y)?"Robin":"Rahul";
+
+    cout<<a;
+    
+    return 0;
+}
