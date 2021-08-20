fada3f56c92af400989781243b9a8b30344b0e90
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-08-20 22:46:27 +0530

    Create Functions - Basic.cpp

---
+++ Functions & Strings/Functions - Basic.cpp
@@ -0,0 +1,23 @@
+/*
+#include<iostream>
+using namespace std;
+*/
+
+// Your Code goes here
+
+int compute(int,int);
+
+int compute(int a,int b){
+    return (a*a)+(b*b);
+}
+
+
+
+/*
+int main()  {
+    int A, B;
+    cin>>A>>B;
+    cout<<compute(A, B)<<endl;
+    return 0;
+}
+*/
